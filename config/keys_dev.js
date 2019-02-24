24a687c06fafaa8e54bc5ca556d192e90491bf88
Jon Varner <jon.varner@gmail.com>
2019-02-24 14:49:43 -0800

    Added express server and moved react app to client subdirectory

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://jon:Alw4ysY3ll0w@alien-empires-bot-rce4e.mongodb.net/test?retryWrites=true',
+  secretOrKey: 'worf'
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+const port = process.env.PORT || 5000;
+
+/* Serve static assets in production */
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
