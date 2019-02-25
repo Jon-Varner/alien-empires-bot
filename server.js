075ad274a84b37d38a8c346ec38821dbd46877eb
Jon Varner <jon.varner@gmail.com>
2019-02-25 14:36:48 -0800

    updated heroku postbuild scripts

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ const port = process.env.PORT || 5000;
 
 /* Serve static assets in production */
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
